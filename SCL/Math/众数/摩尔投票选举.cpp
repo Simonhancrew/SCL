e2c8fc956084b3cc08f35ab83946448fd2e5a2c9
simonhancrew <597494370@qq.com>
2021-08-09 13:41:58 +0800

    摩尔投票选举

---
+++ SCL/Math/众数/摩尔投票选举.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+const int N = 1e5 + 10;
+// 用于选举出出现次数超过数组长度一半的数
+int a[N];
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    int k = 0,candy = 0;
+    for(int i = 0;i < n;i++){
+        int x;
+        cin >> x;
+        a[i] = x;
+        if(k == 0){
+            candy = x;
+            k = 1;
+        }else{
+            if(candy == x){
+                k++;
+            }else{
+                k--;
+            }
+        }
+    }
+    int k = 0;
+    for(int i = 0;i < n;i++){
+        if(a[i] == candy) k++;
+    }
+    if(k > n / 2) cout << candy << endl;
+    else puts("NO");
+    return 0;
+}
