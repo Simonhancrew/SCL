f3926a2b15162c29b1593de4ae1e69ab5beb0941
simonhancrew <597494370@qq.com>
2021-08-29 00:52:40 +0800

    QAQ

---
+++ Competition/Acwing/11/凑平方.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        
+    }
+} 
