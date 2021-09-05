39aa845e47270f7bb59e100f968759bedad663df
simonhancrew <597494370@qq.com>
2021-09-05 23:29:46 +0800

    同组的问题

--- Competition/Leetcode/257/Leetcode5864-游戏中弱角色的数量.h
+++ Competition/Leetcode/257/Leetcode5864-游戏中弱角色的数量.h
@@ -16,6 +16,7 @@ public:
             return lhs[0] > rhs[0] ||(lhs[0] == rhs[0] && lhs[1] < rhs[1]);
         });
         int mx = -1,res = 0, n = p.size();
+        // 因为攻击相同的时候，防守是从小到大的。必不可能出现同组误判
         for(int i = 0;i < n;i++){
             if(p[i][1] < mx) res++;
             else mx = p[i][1];
