3ea28fd8f5d5233411341283c85667e4b9fc64d5
simonhancrew <597494370@qq.com>
2021-09-11 14:48:49 +0800

    dp:数字三角形模型init

---
+++ SCL/DynamicProgramming/线性dp/数字三角形模型/摘花生.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+// 数字三角形的二维矩阵扩展，只能往两个方向走，不重不漏的套用三角形模型就可以了
+
+using namespace std;
+
+const int N = 110;
+
+int dp[N][N],a[N][N];
+
+int r,c;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        cin >> r >> c;
+        memset(dp,0,sizeof dp);
+        for(int i = 1;i <= r;i++){
+            for(int j = 1;j <= c;j++){
+                cin >> a[i][j];
+            }
+        }
+        dp[1][1] = a[1][1];
+        for(int i = 1;i <= r;i++){
+            for(int j = 1;j <= c;j++){
+                dp[i][j] = max(dp[i - 1][j],dp[i][j - 1]) + a[i][j];
+            }
+        }
+        cout << dp[r][c] << endl;
+    }
+    return 0;
+}
