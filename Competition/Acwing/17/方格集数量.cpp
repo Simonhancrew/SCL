5cc7952d2e811f969e5f2674c4c7d63d4d76b7a1
simonhancrew <597494370@qq.com>
2021-09-18 22:37:19 +0800

    周赛总结

--- Competition/Acwing/17/方格集数量.cpp
+++ Competition/Acwing/17/方格集数量.cpp
@@ -55,3 +55,50 @@ int main()
 }
 #endif
 
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+
+using namespace std;
+
+typedef long long LL;
+
+const int N = 60;
+
+int a[N][N];
+int row[N][2];
+int col[N][2];
+// LL C[N][N];
+
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    // init();
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++){
+            cin >> a[i][j]; 
+            if(a[i][j] == 1){
+                row[i][1]++;
+                col[j][1]++;
+            }else{
+                row[i][0]++;
+                col[j][0]++;
+            }
+        }
+    }
+    LL res = 0;
+    // 每个节点可以选，也可以不选，结果就是2 ** n,因为不能是空集，所以要-1
+    for(int i = 0;i < n;i++){
+        res += (1ll << row[i][1]) - 1;
+        res += (1ll << row[i][0]) - 1;
+    }
+    for(int i = 0;i < m;i++){
+        res += (1ll << col[i][1]) - 1;
+        res += (1ll << col[i][0]) - 1;
+    }
+    res -= n * m;
+    cout << res << endl;
+}
