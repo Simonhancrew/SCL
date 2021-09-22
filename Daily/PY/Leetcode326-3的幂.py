23f39ea3115dd367dbf83f06616a8cf0cc1dd46c
simonhancrew <597494370@qq.com>
2021-09-23 00:26:16 +0800

    daily:快速幂 + 分解质因数

--- Daily/PY/Leetcode326-3的幂.py
+++ Daily/PY/Leetcode326-3的幂.py
@@ -1,5 +1,12 @@
+'''
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n == 0:
             return False
         return n & (-n) == n
+'''
+
+# 快速幂
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 and 3 ** 19 % n == 0
--- Daily/sbgo/Leetcode326-3的幂.go
+++ Daily/sbgo/Leetcode326-3的幂.go
@@ -1,5 +1,6 @@
 package sbgo
 
+/*
 func isPowerOfThree(n int) bool {
 	if n == 0 {
 		return false
@@ -8,4 +9,21 @@ func isPowerOfThree(n int) bool {
 		n /= 3
 	}
 	return n == 1
+}
+*/
+
+func isPowerOfThree(n int) bool {
+	return n > 0 && qmi(3, 19)%n == 0
+}
+
+func qmi(base, p int) int {
+	res := 1
+	for p != 0 {
+		if p&1 == 1 {
+			res *= base
+		}
+		base *= base
+		p >>= 1
+	}
+	return res
 }
