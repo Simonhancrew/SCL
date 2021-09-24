0a1cbffe259fac96997cecf2a59c1becd6689b72
simonhancrew <597494370@qq.com>
2021-09-24 10:40:07 +0800

    递归一次

--- Daily/CPP/Leetcode430-扁平化多级双向链表.h
+++ Daily/CPP/Leetcode430-扁平化多级双向链表.h
@@ -20,3 +20,33 @@ public:
         return head;
     }   
 };
+
+#ifdef ONECEDFS
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        dfs(head);
+        return head;
+    }
+    Node* dfs(Node* head){
+        if(!head) return nullptr;
+        auto t = head,last = head;
+        while(t){
+            if(!t->child){
+                last = t;
+                t = t->next;
+            }else{
+                auto ne = t->next,childlast = dfs(t->child);
+                t->next = t->child;
+                t->child->prev = t;
+                t->child = nullptr;
+                t = childlast;
+                t->next = ne;
+                if(ne) ne->prev = t;
+                last = t;  
+            }
+        }
+        return last;
+    }
+};
+#endif
