f964b8b0d9ce143f085bf3986bacf4a42cfce877
simonhancrew <597494370@qq.com>
2021-09-25 23:41:13 +0800

    贪心 + LIS模型

---
+++ SCL/DynamicProgramming/线性dp/最长上升子序列模型/友好城市.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 5e3 + 10;
+
+typedef pair<int,int> PII;
+
+// 代码抽象出来不难，思路比较难
+/*
+    桥梁不能相交，所以先把任意河岸一边的排序，为了保持对应关系，存pair就行了
+    对岸的要不冲突，就刚好是一个上升序列
+    可以转化成一个LIS模型
+*/
+
+PII a[N];
+int dp[N];
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 0;i < n;i++) scanf("%d%d",&a[i].first,&a[i].second);
+    sort(a,a + n);
+    
+    int res = 0;
+    for(int i = 1;i <= n;i++){
+        dp[i] = 1;
+        for(int j = 1;j < i;j++){
+            if(a[i - 1].second > a[j - 1].second){
+                dp[i] = max(dp[i],dp[j] + 1);
+            }
+        }
+        res = max(res,dp[i]);
+    }
+    cout << res << endl;
+}
