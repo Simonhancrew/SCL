dcab81de946d28c24bd85125343ece7eca80c21f
simonhancrew <597494370@qq.com>
2021-09-26 09:25:14 +0800

    LIS转换最大上升和

---
+++ SCL/DynamicProgramming/线性dp/最长上升子序列模型/最大上升子序和.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1e3 + 10;
+int a[N],dp[N];
+int n;
+
+int main()
+{
+    cin >> n;
+    int res = 0;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    for(int i = 1;i <= n;i++){
+        dp[i] = a[i];
+        for(int j = 1;j < i;j++){
+            if(a[i] > a[j]){
+                dp[i] = max(dp[i],dp[j] + a[i]);
+            }
+        }
+        res = max(res,dp[i]);
+    }
+    cout << res << endl;
+    return 0;
+}
