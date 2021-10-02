c45cb7c3779987da0bb902f6e3339345a53e0443
simonhancrew <597494370@qq.com>
2021-10-02 21:09:51 +0800

    多重背包和单调队列优化

--- SCL/DynamicProgramming/背包问题/多重背包问题1.cpp
+++ SCL/DynamicProgramming/背包问题/多重背包问题1.cpp
@@ -6,7 +6,11 @@
 
 using namespace std;
 
+// 二维的比较单一
 const int N = 110;
+
+#ifdef TWODIM
+
 int v[N],w[N],s[N];
 int dp[N][N];
 
@@ -25,4 +29,25 @@ int main()
     }
     cout << dp[n][m] << endl;
     return 0;
+}
+#endif
+
+// 因为当前行之和上一行有关系，所以参考01背包的优化，可以优化成反向遍历体积的一位转移方程
+
+int dp[N];
+int v[N],w[N],s[N];
+int n,m;
+
+int main(){
+    cin >> n >> m;
+
+    for(int i = 0;i < n;i++){
+        int v,w,s;
+        cin >> v >> w >> s;
+        for(int j = m;j >= 0 ;j--){
+            for(int k = 1;k <= s && k * v <= j;k++)
+                dp[j] = max(dp[j],dp[j - k * v] + w * k);
+        }
+    }
+    cout << dp[m] << endl;
 }
---
+++ SCL/DynamicProgramming/背包问题/多重背包问题3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+const int N = 2e4 + 10;
+
+int q[N],g[N],f[N];
+int n,m;
+
+// 旨在用单调队列优化多重背包问题
+/*
+    比较经典的男人八题，首先回忆一下怎么解多重背包问题
+    dp[i][j]的意义就是前i类物品，容积为j的情况下的最大装载利润
+    则不难知道dp[i][j] = max(dp[i - 1][j - v] + w，dp[i - 1][j - 2v] + 2w,...,dp[i - 1[j - s * v] + s * w])
+    这里不难想到一维的dp方案，只要堆每个i，反向枚举dp[m]->dp[0],遍历所有的i，自然能够得到dp[m]的正确解
+    之后继续优化，不难想到，dp[m] = max(dp[m], dp[m-v] + w, dp[m-2*v] + 2*w, dp[m-3*v] + 3*w, ...)
+    接下来，dp[0]->dp[m]可以写成如下的形式
+    dp[0], dp[v],   dp[2*v],   dp[3*v],   ... , dp[k*v]
+    dp[1], dp[v+1], dp[2*v+1], dp[3*v+1], ... , dp[k*v+1]
+    dp[2], dp[v+2], dp[2*v+2], dp[3*v+2], ... , dp[k*v+2]
+    ...
+    dp[j], dp[v+j], dp[2*v+j], dp[3*v+j], ... , dp[k*v+j]
+    显而易见，m 一定等于 k*v + j，其中  0 <= j < v
+    所以，我们可以把 dp 数组分成 j 个类，每一类中的值，都是在同类之间转换得到的
+    也就是说，dp[k*v+j] 只依赖于 { dp[j], dp[v+j], dp[2*v+j], dp[3*v+j], ... , dp[k*v+j] }
+    所以，我们可以把 dp 数组分成 j 个类，每一类中的值，都是在同类之间转换得到的
+    也就是说，dp[k*v+j] 只依赖于 { dp[j], dp[v+j], dp[2*v+j], dp[3*v+j], ... , dp[k*v+j] }
+    所以，我们可以得到
+    dp[j]    =     dp[j]
+    dp[j+v]  = max(dp[j] +  w,  dp[j+v])
+    dp[j+2v] = max(dp[j] + 2w,  dp[j+v] +  w, dp[j+2v])
+    dp[j+3v] = max(dp[j] + 3w,  dp[j+v] + 2w, dp[j+2v] + w, dp[j+3v])
+    ...
+    但是，这个队列中前面的数，每次都会增加一个 w ，所以我们需要做一些转换
+    dp[j]    =     dp[j]
+    dp[j+v]  = max(dp[j], dp[j+v] - w) + w
+    dp[j+2v] = max(dp[j], dp[j+v] - w, dp[j+2v] - 2w) + 2w
+    dp[j+3v] = max(dp[j], dp[j+v] - w, dp[j+2v] - 2w, dp[j+3v] - 3w) + 3w
+    ...
+    这样，每次入队的值是 dp[j+k*v] - k*w
+*/
+
+int main(){
+    cin >> n >> m;
+    for(int i = 0;i < n;i++){
+        int v,w,s;
+        cin >> v >> w >> s;
+        memcpy(g,f,sizeof f);
+        for(int j = 0;j < v;j++){
+            int hh = 0,tt= -1;
+            for(int k = j;k <= m;k += v){
+                if(hh <= tt && q[hh] < k - s * v) hh++;
+                while(hh <= tt && g[q[tt]] - (q[tt] - j) / v * w <= g[k] - (k - j) / v * w ) --tt;
+                q[++tt] = k;
+                f[k] = g[q[hh]] + (k - q[hh]) / v * w;
+            }
+        }
+    }
+    cout << f[m] << endl;
+}
