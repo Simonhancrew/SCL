c944d9087eea3e99b3e1c074025714e40461276c
simonhancrew <597494370@qq.com>
2021-10-05 21:18:12 +0800

    二位费用背包打板

---
+++ SCL/DynamicProgramming/背包问题/二维费用背包问题.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+// 多一维度记录多的要求就行了
+
+const int N = 1e2 + 10;
+int dp[N][N];
+int n,V,M;
+
+int main()
+{
+    cin >> n >> V >> M;
+    int v,m,w;
+    for(int i = 0;i < n;i++){
+        cin >> v >> m >> w;
+        for(int j = V;j >= v;j--){
+            for(int k = M;k >= m;k--){
+                dp[j][k] = max(dp[j][k],dp[j - v][k - m] + w);
+            }
+        }
+    }
+    cout << dp[V][M] << endl;
+}
