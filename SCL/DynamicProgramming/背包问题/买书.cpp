ab417d9e9afd69de218b9f6120fd119844247107
simonhancrew <597494370@qq.com>
2021-10-11 14:14:46 +0800

    完全方案计数背包代码推导

--- SCL/DynamicProgramming/背包问题/买书.cpp
+++ SCL/DynamicProgramming/背包问题/买书.cpp
@@ -28,7 +28,28 @@ int main(){
 }
 #endif
 
+// 推公式，可以优化掉第三重循环，参见完全背包
+
+#if 0
+int a[5] = {0,10,20,50,100};
+int dp[5][N];
+int n;
+
+int main(){
+    cin >> n;
+    dp[0][0] = 1;
+    for(int i = 1;i <= 4;i++){
+        for(int j = 0;j <= n;j++){
+            dp[i][j] = dp[i - 1][j];
+            if(j >= a[i]) dp[i][j] += dp[i][j - a[i]];
+        }
+    }
+    cout << dp[4][n] << endl;
+}
+#endif
+
 // 推公式优化，和数字组合类似的问题解法
+// 因为只用到了前后两个维度，其实只用开一维就可以了
 
 int dp[N];
 
