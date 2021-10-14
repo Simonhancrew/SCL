9042ab6570ab758043df0bbbdf756de671bed2a8
simonhancrew <597494370@qq.com>
2021-10-14 09:59:45 +0800

    daily:b search

--- Daily/CPP/Leetcode852-山脉数组的顶峰索引.h
+++ Daily/CPP/Leetcode852-山脉数组的顶峰索引.h
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+#if 0
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
@@ -11,6 +12,20 @@ public:
             if(arr[mid] > arr[mid + 1]) r = mid;
             else l = mid + 1;
         }
+        return l;
+    }
+};
+#endif
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l = 1,r = arr.size() - 2;
+        while(l < r){
+            int mid = l + r >> 1;
+            if(arr[mid] < arr[mid + 1]) l = mid + 1;
+            else r = mid;
+        }        
         return l;
     }
 };
