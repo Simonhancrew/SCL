cbd4b2f0609682f6b23e4d00ce463383cc0f6203
simonhancrew <597494370@qq.com>
2021-10-18 12:32:56 +0800

    01背包选取

---
+++ SCL/DynamicProgramming/背包问题/开心的金明.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int M = 30010;
+
+// 简单01背包选取就可以了
+
+int dp[M];
+
+int main(){
+    int n,m;
+    cin >> m >> n;
+    for(int i = 1;i <= n;i++){
+        int v,w;
+        cin >> v >> w;
+        w *= v;
+        for(int j = m;j >= v;j--){
+            dp[j] = max(dp[j],dp[j - v] + w);
+        }
+    }
+    cout << dp[m] << endl;
+}
