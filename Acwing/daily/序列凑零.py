f00902d41c51e843a5d414a90cb2a074a961859c
simonhancrew <597494370@qq.com>
2021-10-24 13:43:08 +0800

    QAQ

--- Acwing/daily/序列凑零.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-
-for i in range(t):
-    n = int(input())
-    a = list(map(int,input().split()))
-    for i in range(0,n,2):
-        print(-a[i + 1],end = ' ')
-        print(a[i],end = ' ')
-    print('')
