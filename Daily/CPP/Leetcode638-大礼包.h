55088b727aac64544332a6a1b603031b8a987035
simonhancrew <597494370@qq.com>
2021-10-24 16:25:34 +0800

    QAQ

--- Daily/CPP/Leetcode638-大礼包.h
+++ Daily/CPP/Leetcode638-大礼包.h
@@ -1,48 +1,57 @@
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
+
+// 因为礼包可以无数次选，实际需要的物品是有限制的可以看作一个完全背包问题
+// 背包体积就是总共物品的多少dp[x][y] = min(dp[x - 1][y],dp[x][y - v] + w)
+// 物品最多拿10个，实际做的话需要开10维，可以考虑一个压缩，16进制
+// 用一个记忆化搜索来减少计算
+
+
 class Solution {
 public:
+    vector<unordered_map<int,int>> f;
+    vector<vector<int>> sp;
+    vector<int> pr;
     int n;
-    vector<unordered_map<int, int>> f;
-    vector<int> price;
-    vector<vector<int>> special;
-
-    int dp(int x, int y) {
-        if (f[x].count(y)) return f[x][y];
-        if (!x) {
-            f[x][y] = 0;
-            for (int i = 0; i < n; i ++ ) {
+    // 从前x个礼包中选，凑成物品状态y的最小价值
+    int dp(int x,int y){
+        if(f[x].count(y)) return f[x][y];
+        if(!x) {
+            for(int i = 0;i < n;i++){
                 int c = y >> i * 4 & 15;
-                f[x][y] += c * price[i];
+                f[x][y] += c * pr[i];
             }
             return f[x][y];
         }
-        f[x][y] = dp(x - 1, y);
-        int state = 0;
-        auto s = special[x - 1];
-        for (int i = n - 1; i >= 0; i -- ) {
-            int c = y >> i * 4 & 15;
-            if (c < s[i]) {
-                state = -1;
+        // 完全背包
+        f[x][y] = dp(x - 1,y);
+        int st = 0; // 更新之后的状态
+        auto s = sp[x - 1]; // 当前礼包
+        for(int i = n - 1;i >= 0;i--){
+            int c = y >> i * 4 & 15; // 当前状态下需要多少个物品i
+            if(c < s[i]) { // 礼包不能恰好凑到这个值
+                st = -1;
                 break;
             }
-            state = state * 16 + c - s[i];
+            st = st * 16 + c - s[i];
         }
-        if (state != -1)
-            f[x][y] = min(f[x][y], dp(x, state) + s.back());
+        if(st != -1) {
+            f[x][y] = min(f[x][y],dp(x,st) + s.back());
+        }
         return f[x][y];
     }
-
-    int shoppingOffers(vector<int>& _price, vector<vector<int>>& _special, vector<int>& needs) {
-        price = _price, special = _special;
-        n = price.size();
-        f = vector<unordered_map<int, int>>(special.size() + 1, unordered_map<int, int>());
+    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+        n = needs.size();
+        pr = price,sp = special;
         int state = 0;
-        for (int i = needs.size() - 1; i >= 0; i -- )
+        f.resize(special.size() + 1,unordered_map<int,int> ());
+        for(int i = n - 1;i >= 0;i--){ // 16进制压缩
             state = state * 16 + needs[i];
-        return dp(special.size(), state);
+        }
+        return dp(sp.size(),state);
     }
 };
