3564b148f65a5f02a0396f6c590e6306e9d99a37
simonhancrew <597494370@qq.com>
2021-10-24 15:59:41 +0800

    better solution

--- Daily/CPP/Leetcode638-大礼包.h
+++ Daily/CPP/Leetcode638-大礼包.h
@@ -3,49 +3,46 @@
 
 using namespace std;
 
-const int N = 1771561;
-const int INF = 1000000000;
-
 class Solution {
-private:
-    int f[N];
 public:
-    int shoppingOffers(vector<int>& price, vector<vector<int>>& special,
-        vector<int>& needs) {
+    int n;
+    vector<unordered_map<int, int>> f;
+    vector<int> price;
+    vector<vector<int>> special;
 
-        const int n = needs.size();
-
-        vector<int> mul(n + 1);
-        mul[0] = 1;
-        for (int i = 0; i < n; i++)
-            mul[i + 1] = mul[i] * (needs[i] + 1);
-
-        f[0] = 0;
-        for (int s = 1; s < mul[n]; s++) {
-            f[s] = INF;
-            vector<int> c(n);
-            for (int i = 0; i < n; i++)
-                c[i] = s % mul[i + 1] / mul[i];
-
-            for (int i = 0; i < n; i++)
-                if (c[i] > 0)
-                    f[s] = min(f[s], f[s - mul[i]] + price[i]);
-
-            for (const auto &offer : special) {
-                int t = s;
-                for (int i = 0; i < n; i++)
-                    if (c[i] >= offer[i]) {
-                        t -= offer[i] * mul[i];
-                    } else {
-                        t = -1;
-                        break;
-                    }
-
-                if (t != -1)
-                    f[s] = min(f[s], f[t] + offer[n]);
+    int dp(int x, int y) {
+        if (f[x].count(y)) return f[x][y];
+        if (!x) {
+            f[x][y] = 0;
+            for (int i = 0; i < n; i ++ ) {
+                int c = y >> i * 4 & 15;
+                f[x][y] += c * price[i];
             }
+            return f[x][y];
         }
+        f[x][y] = dp(x - 1, y);
+        int state = 0;
+        auto s = special[x - 1];
+        for (int i = n - 1; i >= 0; i -- ) {
+            int c = y >> i * 4 & 15;
+            if (c < s[i]) {
+                state = -1;
+                break;
+            }
+            state = state * 16 + c - s[i];
+        }
+        if (state != -1)
+            f[x][y] = min(f[x][y], dp(x, state) + s.back());
+        return f[x][y];
+    }
 
-        return f[mul[n] - 1];
+    int shoppingOffers(vector<int>& _price, vector<vector<int>>& _special, vector<int>& needs) {
+        price = _price, special = _special;
+        n = price.size();
+        f = vector<unordered_map<int, int>>(special.size() + 1, unordered_map<int, int>());
+        int state = 0;
+        for (int i = needs.size() - 1; i >= 0; i -- )
+            state = state * 16 + needs[i];
+        return dp(special.size(), state);
     }
 };
