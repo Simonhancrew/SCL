4e5f2ada6d6c6900b7f4430db48f4f6e1226892f
simonhancrew <597494370@qq.com>
2021-11-25 12:44:37 +0800

    cf

---
+++ CodeForces/Solutions/1447A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+// Created by Simonhancrew on 2021/11/25
+
+using namespace std;
+
+int t,n;
+
+/*
+    转化成substract就可以了
+*/
+
+int main(){
+    cin >> t;
+    while(t--){
+        cin >> n;
+        int res = 0;
+        cout << n << endl;
+        for(int i = 1;i <= n;i++) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
