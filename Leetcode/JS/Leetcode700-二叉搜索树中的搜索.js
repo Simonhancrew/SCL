bdfbff6f3efe410c4e2fe10682955dc0dde4a168
simonhancrew <597494370@qq.com>
2021-11-26 13:00:20 +0800

    BST

---
+++ Leetcode/JS/Leetcode700-二叉搜索树中的搜索.js
@@ -0,0 +1,6 @@
+var searchBST = function(root, val) {
+    if(root == null || root.val == val) return root;
+    if(root.val > val) return searchBST(root.left,val);
+    if(root.val < val) return searchBST(root.right,val);
+    return null;
+};
---
+++ Leetcode/sbgo/Leetcode700-搜索二叉树中的搜索.go
@@ -0,0 +1,14 @@
+package sbgo
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil || root.Val == val {
+		return root
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+	if root.Val < val {
+		return searchBST(root.Right, val)
+	}
+	return nil
+}
