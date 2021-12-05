7f8d29c19b698a9dae826e8b3bfd27b066334f80
simonhancrew <597494370@qq.com>
2021-12-05 13:24:02 +0800

    Add C Solution

--- Competition/Leetcode/270/C.h
+++ Competition/Leetcode/270/C.h
@@ -1,3 +1,4 @@
+#if 0 
 class Solution {
 public:
     string rec;
@@ -32,3 +33,23 @@ public:
         path.pop_back();
     }
 };
+#endif 
+
+#ifdef BETTER 
+class Solution {
+public:
+    string path, paths, pathd;
+    void dfs(TreeNode* root, int s, int d) {
+        if(root->val == s) paths = path;
+        if(root->val == d) pathd = path;
+        if(root->right) path += 'R', dfs(root->right, s, d), path.pop_back();
+        if(root->left) path += 'L', dfs(root->left, s, d), path.pop_back();
+    }
+    string getDirections(TreeNode* root, int s, int d) {
+        dfs(root, s, d);
+        int i = 0, j = 0;
+        while(paths[i] == pathd[j]) i++, j++;
+        return string(paths.size() - i, 'U') + pathd.substr(j);
+    }
+};
+#endif 
