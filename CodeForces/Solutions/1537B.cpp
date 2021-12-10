ed419cb3236b1d23d3f32c91c2660c4f7c79fdf4
simonhancrew <597494370@qq.com>
2021-12-10 19:46:26 +0800

    greedy

---
+++ CodeForces/Solutions/1537B.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+// Created by Simonhancrew on 2021/12/10
+
+using namespace std;
+
+// 放对角线就可以了
+
+int t,n,m,x,y;
+
+int main(){
+    cin >> t;
+    while(t--){
+        cin >> n >> m >> x >> y;
+        cout << 1 << ' ' << 1 << ' ' << n << ' ' <<  m << '\n';
+    }
+    return 0;
+}
