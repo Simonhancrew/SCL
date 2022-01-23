63a1fd0516794cd6e53e100e73648d9ad0e4349c
simonhancrew <597494370@qq.com>
2022-01-23 18:00:59 +0800

    枚举顺序

---
+++ SCL/Graph/DFS/搜索顺序/组合型枚举.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 30;
+
+int m,n;
+int path[N];
+
+// 保证顺序枚举
+void dfs(int start,int cnt) {
+    if(cnt == m) {
+        for(int i = 0;i < m;i++) cout << path[i] << ' ';
+        cout << endl;
+    }else{
+        for(int i = start;i <= n;i++) {
+            path[cnt] = i;
+            dfs(i + 1,cnt + 1);
+            path[cnt] = 0;
+        }
+    }
+}
+
+int main(){
+    cin >> n >> m;
+    dfs(1,0);
+    return 0;
+}
