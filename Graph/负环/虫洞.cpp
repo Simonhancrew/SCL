b20a2372a6d257ab0f5071c4821c5e7640e9925c
simonhancrew <597494370@qq.com>
2022-05-03 23:00:02 +0800

    spfa求负环

---
+++ Graph/负环/虫洞.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+
+// Created by Simonhancrew on 2022/05/03
+
+using namespace std;
+
+/*
+    很裸的一道求负环的题目
+    直接spfa做就行
+*/
+
+typedef long long LL;
+typedef pair<int, int> PII;
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+
+const int INF = 0x3f3f3f3f, N = 510, M = 5210;
+
+int n, m1, m2, t;
+int cnt[N], dist[N];
+bool st[N];
+int h[N], e[M], ne[M], w[M], idx;
+int q[N];
+
+void add(int a, int b, int c)
+{
+    e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx++;
+}
+
+bool spfa()
+{
+    memset(st, 0, sizeof st);
+    memset(dist, 0, sizeof dist);
+    memset(cnt, 0, sizeof cnt);
+    int hh = 0, tt = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        q[tt++] = i;
+        st[i] = true;
+    }
+
+    while (hh != tt)
+    {
+        int t = q[hh++];
+        if (hh == N)
+            hh = 0;
+        st[t] = false; // 和dij的逻辑不一样，出队不能确定最小，只是不用松弛了
+        for (int i = h[t]; i != -1; i = ne[i])
+        {
+            int j = e[i];
+            if (dist[j] > dist[t] + w[i])
+            {
+                dist[j] = dist[t] + w[i];
+                cnt[j] = cnt[t] + 1;
+                if (cnt[j] >= n)
+                    return true;
+                if (!st[j])
+                {
+                    st[j] = true;
+                    q[tt++] = j;
+                    if (tt == N)
+                        tt = 0;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    fast_cin();
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m1 >> m2;
+        idx = 0;
+        memset(h, -1, sizeof h);
+        while (m1--)
+        {
+            int a, b, c;
+            cin >> a >> b >> c;
+            add(a, b, c), add(b, a, c);
+        }
+        while (m2--)
+        {
+            int a, b, c;
+            cin >> a >> b >> c;
+            add(a, b, -c);
+        }
+
+        if (spfa())
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
