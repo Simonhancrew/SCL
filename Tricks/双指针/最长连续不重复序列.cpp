af36897fc1cf60e82238f96d79ba9538fcc0d27f
simonhancrew <597494370@qq.com>
2022-05-05 11:16:33 +0800

    Summary Sliding Window

--- Tricks/双指针/最长连续不重复序列.cpp
+++ Tricks/双指针/最长连续不重复序列.cpp
@@ -3,27 +3,32 @@ using namespace std;
 
 const int N = 1e5 + 10;
 
-int a[N],r[N];
+int a[N], r[N];
 
-
-//枚举区间右端点,对于每一个右端点去找他的最左端点
-
-int main(){
+/*
+    枚举区间右端点,对于每一个右端点去找他的最左端点
+    Sliding Window应用题
+*/
+int main()
+{
     int n;
-    scanf("%d",&n);
-    for(int i = 0;i < n;i++){
-        scanf("%d",&a[i]);
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
     }
     int ans = 1;
-    for(int i=0,j = 0;i < n;++i){
+    for (int i = 0, j = 0; i < n; ++i)
+    {
         //前一个区间结束时没有重复的数的
         //新加一个数a[i]进来如果有重复的一定是a[i]
         r[a[i]]++;
-        while(r[a[i]] > 1){
+        while (r[a[i]] > 1)
+        {
             r[a[j]]--;
             j++;
         }
-        ans = max(ans,i - j + 1);
+        ans = max(ans, i - j + 1);
     }
     cout << ans;
     return 0;
