d6a7649eec75a604c339f0188f970c27fe69fcab
simonhancrew <597494370@qq.com>
2022-07-26 23:40:00 +0800

    modify skip list

---
+++ Datastruct/跳表/skip_list.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include <random>
+#include <iostream>
+using namespace std;
+/*
+    一个跳表的实现。一个节点可以理解成一个矩形区域，会按照一定概率覆盖一个高度
+*/
+static const int kMaxLevel = 8;
+
+struct Node
+{
+    int val_;
+    vector<Node *> next_;
+    Node(int val) : val_(val), next_(kMaxLevel, nullptr) {}
+};
+
+class Skiplist
+{
+public:
+    Node *head_;
+    Skiplist()
+    {
+        head_ = new Node(-1);
+    }
+
+    vector<Node *> find(int target)
+    {
+        vector<Node *> pre(kMaxLevel, nullptr);
+        auto p = head_;
+        for (int i = kMaxLevel - 1; i >= 0; i--)
+        {
+            while (p->next_[i] && p->next_[i]->val_ < target)
+                p = p->next_[i];
+            pre[i] = p;
+        }
+        return pre;
+    }
+
+    bool search(int target)
+    {
+        auto pre = find(target);
+        auto p = pre[0]->next_[0];
+        return p && p->val_ == target;
+    }
+
+    void add(int num)
+    {
+        auto pre = find(num);
+        auto p = new Node(num);
+        for (int i = 0; i < kMaxLevel; i++)
+        {
+            p->next_[i] = pre[i]->next_[i];
+            pre[i]->next_[i] = p;
+            if (rand() % 2)
+                break;
+        }
+    }
+
+    bool erase(int num)
+    {
+        auto pre = find(num);
+        auto p = pre[0]->next_[0];
+        if (!p || p->val_ != num)
+            return false;
+
+        for (int i = 0; i < kMaxLevel && pre[i]->next_[i] == p; i++)
+        {
+            pre[i]->next_[i] = p->next_[i];
+        }
+        delete p;
+        return true;
+    }
+};
+
+int main()
+{
+    Skiplist slt;
+    slt.add(3);
+    slt.add(5);
+    slt.add(6);
+    cout << slt.search(6) << endl;
+    cout << slt.search(3) << endl;
+    cout << slt.search(5) << endl;
+    slt.erase(5);
+    slt.erase(6);
+    cout << slt.search(6) << endl;
+    cout << slt.search(5) << endl;
+}
