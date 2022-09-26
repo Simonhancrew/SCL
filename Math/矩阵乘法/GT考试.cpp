055af44c6dc18797f11734c3ff102a51f2ed4877
simonhancrew <597494370@qq.com>
2022-09-26 22:57:24 +0800

    init GT Test

---
+++ Math/矩阵乘法/GT考试.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+
+// Created by Simonhancrew on 2022/09/26
+
+using namespace std;
+
+using LL  = long long;
+using PII = pair<int, int>;
+#define fast_cin()                  \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(nullptr);                 \
+  cout.tie(nullptr)
+
+const int INF = 0x3f3f3f3f;
+
+int main() {
+  // freopen("input.txt","r",stdin);
+  // freopen("output.txt","w",stdout);
+  fast_cin();
+
+  return 0;
+}
