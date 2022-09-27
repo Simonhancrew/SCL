363a359fc7853948011fd13e3cbc4401bc02602d
simonhancrew <597494370@qq.com>
2022-09-27 11:21:33 +0800

    mtrix mul for dp

--- Math/矩阵乘法/GT考试.cpp
+++ Math/矩阵乘法/GT考试.cpp
@@ -15,10 +15,67 @@ using PII = pair<int, int>;
 
 const int INF = 0x3f3f3f3f;
 
+const int N = 25;
+
+int n, m, mod;
+char s[N];
+int ne[N];
+int a[N][N];
+
+void mul(int a[][N], int b[][N], int c[][N]) {
+  static int tmp[N][N];
+  memset(tmp, 0, sizeof tmp);
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < m; j++) {
+      for (int k = 0; k < m; k++) {
+        tmp[i][j] = (tmp[i][j] + b[i][k] * c[k][j]) % mod;
+      }
+    }
+  }
+  memcpy(a, tmp, sizeof tmp);
+}
+
+int qmi(int k) {
+  int f[N][N] = {1};
+  while (k) {
+    if (k & 1)
+      mul(f, f, a);
+    mul(a, a, a);
+    k >>= 1;
+  }
+  int res = 0;
+  for (int i = 0; i < m; i++)
+    res = (res + f[0][i]) % mod;
+  return res;
+}
+
 int main() {
   // freopen("input.txt","r",stdin);
   // freopen("output.txt","w",stdout);
   fast_cin();
+  cin >> n >> m >> mod;
+  cin >> s + 1;
 
+  for (int i = 2, j = 0; i <= m; i++) {
+    while (j && s[j + 1] != s[i])
+      j = ne[j];
+    if (s[j + 1] == s[i])
+      j++;
+    ne[i] = j;
+  }
+
+  for (int i = 0; i < m; i++) {
+    for (char c = '0'; c <= '9'; c++) {
+      int k = i;
+      while (k && s[k + 1] != c)
+        k = ne[k];
+      if (s[k + 1] == c)
+        k++;
+      if (k < m)
+        a[i][k]++;
+    }
+  }
+
+  cout << qmi(n) << '\n';
   return 0;
 }
