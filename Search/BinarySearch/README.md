cf551fd66c8fca78fe260f9ff4eff9e3bd6bfa41
simonhancrew <597494370@qq.com>
2023-12-19 22:14:39 +0800

    [search] readme

--- Search/BinarySearch/README.md
+++ Search/BinarySearch/README.md
@@ -1,7 +1,6 @@
-## 二分搜索模板解读
+# 二分搜索模板解读
 
 记得二分区间要表达的意义
-
 
 这种找lower bound的，保证的是l - 1永远不合法，转换过来说就是a[l - 1]永远小于target，a[r]永远大于等于target。
 
@@ -9,81 +8,32 @@
 
 优先下取整，把l缩放到最大
 
-这种写法不会超越下标的边界，也就是说如果不存在这个数需要自己去检查一下
+写开区间的，(l, r), l<=的位置永远小于target，>= r的位置永远大于等于target
 
+此时，如果没有合法解答，r会位于数组右外侧。需要额外判断段一下
 
 ```cpp
-bool check(int x) {/* ... */} // 检查x是否满足某种性质
-
-// 区间[l, r]被划分成[l, mid]和[mid + 1, r]时使用：
-int bsearch_1(int l, int r)
-{
-    while (l < r)
-    {
-        int mid = l + r >> 1;
-        if (check(mid)) r = mid;    // check()判断mid是否满足性质
-        else l = mid + 1;
+void lower_bound(int target) {
+  int l = -1, r = n;
+  while (l + 1 < r) {
+    int mid = (l + r) >> 1;
+    if (check(mid)) {
+        r = mid;
+    } else {
+        l = mid;
     }
-    return l;
-}
-```
-
-这种找上边界的，r + 1保证不合法，也就是r + 1的小标永远大于target, l永远大于等于target
-
-优先上取整，把r的大小缩到最小
-```cpp
-// 区间[l, r]被划分成[l, mid - 1]和[mid, r]时使用：
-int bsearch_2(int l, int r)
-{
-    while (l < r)
-    {
-        int mid = l + r + 1 >> 1;
-        if (check(mid)) l = mid;
-        else r = mid - 1;
-    }
-    return l;
-}
-```
-
-注意l + r >> 1是下取整的，所以实际更新的时候，如果更新的是l，需要考虑边界的问题，否则就会死循环，比如
-
-```cpp
-l = 0,r = 1
-l = mid = (l + r) >> 1 
-```
-
-此时l还是0，完全死循环了。改成`l + r + 1 >> 1`就没有问题了。
-
-另外还有上下界的判断问题，这里记录一下怎么找上界
-
-```cpp
-if(num[mid] <= target) l = mid;
-```
-
-这里每次等于的时候边界也会更新，一直到不能更新的时候找到了恰好找到了target的右边界，l == r,更新结束。简而言之就是左边界会一直往右边缩小。
-
-同理，下界就是.满足条件的时候右边界一直往下缩小。
-
-```cpp
-if(num[mid] >= target) r = mid
-```
-
-另外还有一种写法， 这种保证l-1永远小于target，r + 1永远大于target
-
-因此这种对于不存在的数，会返回一个边界外的坐标n
-```cpp
-int lower_bound(int a[], int tar) {
-  int l = 0,r = n - 1;
-  while (l < r) {
-    int mid = l + r >> 1;
-    if (a[mid] < tar) l = mid + 1;
-    else r = mid - 1;
   }
-  return l;
+  return r;
 }
 ```
 
-### 二段性的二分
+找上界的话，稍微修改一下lower bound
+
+```cpp
+return lower_bound(target + 1) - 1;
+```
+
+## 二段性的二分
 
 二分只要满足二段性就能够使用，将一个二段性数组区间[l, r]看作蓝色和红色的部分(左侧是红色，也就是小于或者不满足条件的区域)
 
--- Search/BinarySearch/binary_search.cpp
+++ Search/BinarySearch/binary_search.cpp
@@ -1,8 +1,9 @@
 #include <algorithm>
 #include <cstring>
 #include <iostream>
+#include <vector>
 
-// Created by Simonhancrew on 2023/06/03
+// Created by Simonhancrew on 2023/12/19
 
 using namespace std;
 
@@ -10,73 +11,44 @@ using LL  = long long;
 using PII = pair<int, int>;
 #define fast_cin()                  \
   ios_base::sync_with_stdio(false); \
-  cin.tie(nullptr);                 \
-  cout.tie(nullptr)
+  cin.tie(NULL);                    \
+  cout.tie(NULL)
 
-const int INF = 0x3f3f3f3f, N = 1e5 + 10;
+const int INF = 0x3f3f3f3f;
 
-int n, q, k;
-int a[N];
+int n, q;
 
-// 第一种写法，比较好按照循环不变量解释，最后的结果要保证l - 1小于target
-// r + 1大于等于target
-// 区间左右闭合
-// 这种写法，如果值不存在会返回一个超越界限下标的n
-int lower_bound(int a[], int tar) {
-  int l = 0, r = n - 1;
-  while (l <= r) {
+int lower_bound(const vector<int> &arr, int target) {
+  auto n = arr.size();
+  int l = -1, r = n;
+  while (l + 1 < r) {
     int mid = l + r >> 1;
-    if (a[mid] < tar)
-      l = mid + 1;
-    else
-      r = mid - 1;
-  }
-  return l;
-}
-
-// 第二种写法，r大于等于tar， l - 1小于target
-// 这种写法不会超出数据范围，值不存在也会卡在左右边界上
-int lower_bound2(int a[], int tar) {
-  int l = 0, r = n - 1;
-  while (l < r) {
-    int mid = l + r >> 1;
-    if (a[mid] >= tar)
+    if (arr[mid] >= target)
       r = mid;
     else
-      l = mid + 1;
-  }
-  return l;
-}
-
-// 这种保证r + 1大于target， l小于等于target
-int upper_bound(int a[], int tar) {
-  int l = 0, r = n - 1;
-  while (l < r) {
-    int mid = l + r + 1 >> 1;
-    if (a[mid] <= tar)
       l = mid;
-    else
-      r = mid - 1;
   }
-  return l;
+  return l + 1;
 }
 
 int main() {
-  // freopen("input.txt","r",stdin);
-  // freopen("output.txt","w",stdout);
   fast_cin();
   cin >> n >> q;
-  for (int i = 0; i < n; i++)
-    cin >> a[i];
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  sort(arr.begin(), arr.end());
   while (q--) {
-    cin >> k;
-    auto lhs = lower_bound(a, k);
-    if (lhs == n || a[lhs] != k) {
-      cout << "-1 -1" << endl;
+    int target;
+    cin >> target;
+    auto l = lower_bound(arr, target);
+    if (l == n || arr[l] != target) {
+      cout << -1 << ' ' << -1 << '\n';
       continue;
     }
-    auto rhs = lower_bound(a, k + 1) - 1;
-    cout << lhs << ' ' << rhs << endl;
+    auto r = lower_bound(arr, target + 1) - 1;
+    cout << l << ' ' << r << '\n';
   }
   return 0;
 }
