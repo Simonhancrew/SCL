2a2546a361bf7a22ae5b409164d158b339b5acb9
simon <597494370@qq.com>
2024-11-12 13:50:02 +0800

    update area dp

--- DynamicProgramming/区间dp/石子合并.cpp
+++ DynamicProgramming/区间dp/石子合并.cpp
@@ -1,40 +1,59 @@
 #include <algorithm>
 #include <iostream>
+#include <climits>
+#include <cstring>
+
+// Created by Simonhancrew on 2024/11/12
 
 using namespace std;
 
-//动态规划还是需要经验，很多直接想出来其实不太可能
-//以分界线作为最后的分堆标准，左边和为一堆，右边另一堆
-//快速处理部分和需要用到前缀和
-//这里从前往后枚举是不对的，需要枚举区间长度（可能多次枚举一个区间长度）
-//或者你逆序枚举，要保证用到的状态之前已经算过了
+/*
+枚举左右端
 
-const int N = 310;
-int dp[N][N];
-int s[N];
+`[l, r]`的合并的代价=`[l, mid]`合并代价 + `[mid + 1, r]`合并代价 + `[l, r]`最后一次合并合成一堆的代价
+
+最后一次的代价可以用前缀和优化
+*/
+
+using LL = long long;
+using PII = pair<int, int>;
+#define fast_cin()                  \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(nullptr);                 \
+  cout.tie(nullptr)
+
+const int INF = 0x3f3f3f3f, N = 310;
+
+int n;
+int a[N];
+int f[N][N];
+
+int dfs(int l, int r) {
+  if (l >= r) {
+    return 0;
+  }
+  auto& res = f[l][r];
+  if (res != -1) {
+    return res;
+  }
+  res = INT_MAX;
+  auto sum = a[r] - a[l - 1];
+  for (int mid = l; mid < r; mid++) {
+    res = min(res, dfs(l, mid) + dfs(mid + 1, r) + sum);
+  }
+  return res;
+}
 
 int main() {
-  int n;
+  // freopen("input.txt","r",stdin);
+  // freopen("output.txt","w",stdout);
+  fast_cin();
   cin >> n;
-  //算一下前缀和
   for (int i = 1; i <= n; i++) {
-    cin >> s[i];
-    s[i] += s[i - 1];
+    cin >> a[i];
+    a[i] += a[i - 1];
   }
-  //按照长度从小到大枚举所有的状态
-  //长度为1的时候合并没有代价，不考虑
-  for (int len = 2; len <= n; len++) {
-    for (int i = 1; i + len - 1 <= n; i++) {
-      //区间做右端点
-      int l = i, r = i + len - 1;
-      //记得初始代价为无穷大，负责dp为0，代价已经最小了
-      dp[l][r] = 1e9;
-      //枚举可以的划分区间
-      for (int k = l; k < r; k++) {
-        dp[l][r] = min(dp[l][r], dp[l][k] + dp[k + 1][r] + s[r] - s[l - 1]);
-      }
-    }
-  }
-  cout << dp[1][n] << endl;
+  memset(f, -1, sizeof f);
+  cout << dfs(1, n) << '\n';
   return 0;
 }
